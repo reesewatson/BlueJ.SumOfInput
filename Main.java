6076ae6fffc08e46832070f1ace82dcfd17b482b
Reese Watson <sherrese.watson@gmail.com>
2019-05-22 13:13:59 -0400

    Updated some stuff.

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+ 
+import java.util.Scanner;
+
 public class Main {
+    private static int sum;
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
-
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter a number: ");
+                int num = input.nextInt();
+                for (int i = 1; i <= num; i++) {
+                    sum += i;
+                }
+        System.out.println("The sum of all digits within " + num + " is: " + sum);
     }
 }
